57acb9272047bb2f06abf4468625fb7e54a7c97b
Teerapat Chaupetch <teerapat.chau@gmail.com>
2019-01-19 23:29:25 +0700

    3.Movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  private Rigidbody2D myRigidbody;
+  [SerializeField]
+  private float movementSpeed;
+
+  void Start() {
+    myRigidbody = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate() {
+    float horizontal = Input.GetAxis("Horizontal");
+    HandleMovement(horizontal);
+  }
+
+  void HandleMovement(float horizontal) {
+    myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+  }
+
+}
