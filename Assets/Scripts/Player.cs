c73dac7080984fed24e7aa72ce7a559177f2f57a
Teerapat Chaupetch <teerapat.chau@gmail.com>
2019-01-19 23:35:44 +0700

    3.Flipping the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,30 @@ public class Player : MonoBehaviour {
   private Rigidbody2D myRigidbody;
   [SerializeField]
   private float movementSpeed;
+  private bool facingRight;
 
   void Start() {
+    facingRight = true;
     myRigidbody = GetComponent<Rigidbody2D>();
   }
 
   void FixedUpdate() {
     float horizontal = Input.GetAxis("Horizontal");
     HandleMovement(horizontal);
+    Flip(horizontal);
   }
 
   void HandleMovement(float horizontal) {
     myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+  }
+
+  private void Flip(float horizontal) {
+    if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight) {
+      facingRight = !facingRight;
+      Vector3 theScale = transform.localScale;
+      theScale.x *= -1;
+      transform.localScale = theScale;
+    }
   }
 
 }
